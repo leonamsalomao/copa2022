610308920f07bac7453a9f0f274d157530fc0433
leo_salomao7 <70729829+leonamsalomao@users.noreply.github.com>
2022-11-28 13:50:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function createGame(player1, hour, player2) {
+function createGame(player1, placar1, hours, placar2, player2) {
   return `
     <li>
       <img
         src="assets/images/icon-${player1}.svg"
         alt="Bandeira do ${player1}"
       />
-      <strong>${hour}</strong>
+      <h3>${placar1}</h3>
+      <strong>x</strong>
+      <h3>${placar2}</h3>
       <img
         src="assets/images/icon-${player2}.svg"
         alt="Bandeira da ${player2}"
@@ -15,11 +17,14 @@ function createGame(player1, hour, player2) {
 }
 
 let delay = -0.4
-function createCard(date, day, games) {
+function createCard(date, hour, day, games) {
   delay = delay + 0.4
   return `
     <div class="card" style="animation-delay: ${delay}s">
-      <h2>${date} <span>${day}</span></h2>
+      <div class="wrapper">
+        <h2>${date} <span>${day}</span></h2>
+        <h3><span>${hour}</span></h3>
+      </div> 
       <ul>
         ${games}
       </ul>
@@ -28,10 +33,6 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('#cards').innerHTML =
-  createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
-  createCard(
-    '28/11',
-    'segunda',
-    createGame('switzerland', '13:00', 'brazil')
-  ) +
-  createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
+  createCard('24/11', '16:00', 'quinta', createGame('brazil', '2', '16:00', '0', 'serbia')) +
+  createCard('28/11', '13:00', 'segunda', createGame('switzerland', '0', '13:00', '0', 'brazil')) +
+  createCard('02/12', '16:00', 'sexta', createGame('brazil', '0', '16:00','0', 'cameroon'))
